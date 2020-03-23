972111ca524556806d83e59cc12c0cefc3751a8a
Emmanuel Leguizamon <62218305+Emalegui95@users.noreply.github.com>
2020-03-23 12:46:12 -0300

    Ejercicio II
    Ejercicio II de los ejercicios de repaso.

---
+++ CAI_EjercicioII/CAI_EjercicioII/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAI_EjercicioII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int registro = 691;
+
+            for (int i = 1; i <= registro; i++)
+            {
+                if (i % 3 == 0 && num_Primo(i))
+                {
+                    Console.WriteLine(i + " - Es primo y es divisible por 3");
+                }
+                else if (num_Primo(i))
+                {
+                    Console.WriteLine(i + " - Es primo");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine(i + " - Es divisible por 3");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey(true);
+        }
+
+        static bool num_Primo(int numero)
+        {
+            bool resultado = true;
+            for (int i = 2; i < numero; i++)
+            {
+                if (numero % 1 == 0)
+                {
+                    resultado = false;
+                }
+            }
+            return resultado;
+
+        }
+    }
+}
